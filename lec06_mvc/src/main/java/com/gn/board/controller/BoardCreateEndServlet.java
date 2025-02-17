6d0eb1491513d3ec3b603b1626aecadca4bb5897
Jayden6849 <jayden6849@gmail.com>
2025-02-17 11:51:01 +0900

    [Jayden] 파일 메타데이터 DB에 저장 작성중 (lec06)
    	- Service 트랜잭션 작성

--- lec06_mvc/src/main/java/com/gn/board/controller/BoardCreateEndServlet.java
+++ lec06_mvc/src/main/java/com/gn/board/controller/BoardCreateEndServlet.java
@@ -3,6 +3,7 @@ package com.gn.board.controller;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +16,7 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+import com.gn.board.service.BoardService;
 import com.gn.board.vo.Attach;
 import com.gn.board.vo.Board;
 
@@ -55,20 +57,64 @@ public class BoardCreateEndServlet extends HttpServlet {
 				
 				if(fileItem.isFormField()) {
 					// 1) 파일 형태가 아닌 form 내부 요소
-					System.out.println(fileItem.getFieldName());
+					// System.out.println(fileItem.getFieldName()); // name 속성(키값)이 찍힘
+					
+					// 각 키값에 따라 구분하여 바구니(Board 객체)에 담아줌
+					switch(fileItem.getFieldName()) {
+					case "board_title":
+						b.setBoardTitle(fileItem.getString("UTF-8"));
+						break;
+					case "board_content":
+						b.setBoardContent(fileItem.getString("UTF-8"));
+						break;
+					case "board_writer":
+						b.setBoardWriter(Integer.parseInt(fileItem.getString("UTF-8")));
+						break;
+					}
 				} else {
 					// 2) 파일 형태인 form 내부 요소
-					System.out.println(fileItem.getName());
+					// System.out.println(fileItem.getName()); // 파일명이 찍힘
+					
+					if(fileItem.getSize() > 0) {
+						// 파일의 내용물이 있을 때만 저장
+						String oriName = fileItem.getName();
+						
+						// 확장자는 UUID로 변경하면 안 되기 때문에 따로 추출
+						int idx = oriName.lastIndexOf(".");
+						String ext = oriName.substring(idx);
+						
+						// UUID 방식으로 숨겨서 저장할 것임
+						String uuid = UUID.randomUUID().toString().replace("-","");
+						String newName = uuid+ext;
+						
+						File uploadFile = new File(dir, newName);
+						fileItem.write(uploadFile);
+						// 파일 데이터를 서버에 저장하는 절차 끝!
+						
+						// 파일 메타데이터를 Attach 바구니에 담아둠
+						a.setOriName(oriName);
+						a.setNewName(newName);
+						a.setAttachPath(path+"\\"+newName); // C:\\upload\\board\\새로운이름.확장자
+					}
 				}
 			}
+			
+			// 바구니에 들어있는 데이터를 확인 - 콘솔창에 찍히는지 확인
+			System.out.println(b);
+			System.out.println(a);		
+			
+			// 지정한 경로에 파일이 잘 업로드 되었는지 확인 - 폴더가서 눈으로 확인
+			
+			// 여기까지 완료되면 파일이 정상적으로 업로드 되었다는 것
+			
+			// 이제 파일 정보를 DB에 업로드해야함 (Board, Attatch 각각의 정보를 insert 하는 트랜잭션 처리해줘야함)
+			int result = new BoardService().createBoard(b, a);
+			
 		} catch (FileUploadException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ lec06_mvc/src/main/java/com/gn/board/service/BoardService.java
@@ -0,0 +1,44 @@
+package com.gn.board.service;
+
+import static com.gn.common.sql.JDBCTemplate.close;
+import static com.gn.common.sql.JDBCTemplate.commit;
+import static com.gn.common.sql.JDBCTemplate.getConnection;
+import static com.gn.common.sql.JDBCTemplate.rollback;
+
+import java.sql.Connection;
+
+import com.gn.board.dao.BoardDao;
+import com.gn.board.vo.Attach;
+import com.gn.board.vo.Board;
+
+public class BoardService {
+	public int createBoard(Board b, Attach a) {
+		Connection conn = getConnection();
+		
+		int result = 0;
+		
+		try {
+			conn.setAutoCommit(false);
+			
+			int boardNo = new BoardDao().insertBoard(conn, b);
+			a.setBoardNo(boardNo);
+			
+			int attachNo = new BoardDao().insertAttach(conn, a);
+			
+			if(boardNo != 0 && attachNo != 0) {
+				// boardNo, attachNo의 insert 동작이 모두 이루어졌을 경우
+				result = 1;
+				commit(conn);
+			} else {
+				// 예외가 발생하진 않았지만 insert 동작이 이루어지지 않은 경우
+				rollback(conn);
+			}
+		} catch(Exception e) {
+			rollback(conn);
+			e.printStackTrace();
+		}
+		close(conn);
+		
+		return result;
+	}
+}
